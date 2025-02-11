8fd38806403543973279b3a63f494014a463307e
Auffter <lukikarpiel@gmail.com>
2025-02-11 19:40:56 +0100

    Added solution for problem 0004

--- src/0004-MedianOfArrays/medianarray.ts
+++ src/0004-MedianOfArrays/medianarray.ts
@@ -1,3 +1,15 @@
 export function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    return 0;
+    const mergedArray = nums1.concat(nums2).sort((a, b) => a - b);
+
+    const n:number = mergedArray.length;
+
+    if(n === 0) {
+        throw new Error('Both arrays are empty');
+    }
+
+    if ((n - 1) % 2 === 0) {
+        return (mergedArray[Math.floor((n / 2))]);
+    } else {
+        return ((mergedArray[Math.floor((n / 2))] + mergedArray[Math.floor((n / 2) - 1)]) / 2);
+    }
 }
