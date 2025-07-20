2bf22b4caca37c63b35cb3f7792a7f5fe0fcf03f
Auffter <lukikarpiel@gmail.com>
2025-07-20 13:51:38 +0200

    Added solution for problem 326

--- src/0326-PowerOfThree/powerofthree.ts
+++ src/0326-PowerOfThree/powerofthree.ts
@@ -1,3 +1,15 @@
 export function isPowerOfThree(n: number): boolean {
+
+    if(n == 1) {
+        return true;
+    }
+
+    // If number n is power of three, power will be an integer
+    let power = (Math.log10(n) / Math.log10(3));
+
+    if(power - Math.floor(power) !== 0) {
+        return false;
+    }
+
     return true;
 }
